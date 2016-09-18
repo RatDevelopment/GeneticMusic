f05ccda8c0420a5442a5770bdd42001a747824fc
Cezary Wojcik <cezarywojcik92@gmail.com>
2016-09-18 01:03:08 -0700

    accumulate coverage results for multiple tests

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -3,6 +3,8 @@
 TESTS_FOLDER="app/test/"
 TEST_FILE_PATTERN="*-tests.py"
 
+coverage erase
+
 for file in $(find $TESTS_FOLDER -name "$TEST_FILE_PATTERN")
 do
   # replace all slashes in the filename with periods
@@ -10,5 +12,5 @@ do
   # remove the remaining `.py` at the end of the filename
   testmodule=${testfile::-3}
   # run the test
-  coverage run -m $testmodule
+  coverage run -am $testmodule
 done
