563e707bec41748d746e3f2a282778417de22da8
Cezary Wojcik <cezarywojcik@users.noreply.github.com>
2016-09-17 23:52:27 -0700

    added some comments for runtests script (#8)

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -5,6 +5,10 @@ TEST_FILE_PATTERN="*-tests.py"
 
 for file in $(find $TESTS_FOLDER -name "$TEST_FILE_PATTERN")
 do
+  # replace all slashes in the filename with periods
   testfile=${file//\//\.}
-  coverage run -m ${testfile::-3}
+  # remove the remaining `.py` at the end of the filename
+  testmodule=${testfile::-3}
+  # run the test
+  coverage run -m $testmodule
 done
